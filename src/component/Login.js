5a99e22fd94cb05758836aede4b253d57029a640
Chisty Md.Muzammel Hossain <chistyz10@gmail.com>
2021-05-04 22:56:37 +0600

    update

--- src/component/Login.js
+++ src/component/Login.js
@@ -82,7 +82,7 @@ export  const  Login = () =>{
 
     return (
      
-        <div className="container">
+        <div>
           <div className="row">
             <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
               <div className="card card-signin my-5">
@@ -91,19 +91,19 @@ export  const  Login = () =>{
                   <form className="form-signin" onSubmit={FormSubmit}>
                     <div className="form-label-group">
                       <input type="text" id="inputUsername" className="form-control" placeholder="Username" 
-                      name="username" value={newUser.username} onChange={changeUser} required autofocus/>
-                      <label for="inputUsername">Username</label>
+                      name="username" value={newUser.username} onChange={changeUser} required autoFocus/>
+                      <label htmlFor="inputUsername">Username</label>
                     </div>
       
                     <div className="form-label-group">
                       <input type="password" id="inputPassword" className="form-control" placeholder="Password" 
                       name="password" value={newUser.password} onChange={changeUser} required/>
-                      <label for="inputPassword">Password</label>
+                      <label htmlFor="inputPassword">Password</label>
                     </div>
       
                     <div className="custom-control custom-checkbox mb-3">
                       <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-                      <label className="custom-control-label" for="customCheck1">Remember password</label>
+                      <label className="custom-control-label" htmlFor="customCheck1">Remember password</label>
                     </div>
                     <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
                     <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Student Registration</button>
