acab1565be35b7d4a3848ff99845afc6fc97e41e
Chisty Md.Muzammel Hossain <chistyz10@gmail.com>
2021-05-04 17:24:17 +0600

    branch

--- src/component/teacher/teacherlayout.js
+++ src/component/teacher/teacherlayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom';
-import './style.css';
+//import './style.css';
 
 export const Teacherlayout = () => {
   return (
